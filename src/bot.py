5d8cb8ba509227ee3b0af0cfe5b61666df91f438
m6un <midhunchandran511@gmail.com>
2021-02-04 10:53:36 +0530

    Initial commit

--- src/bot.py
+++ src/bot.py
@@ -2,33 +2,34 @@ import time
 import random
 import tweepy
 from tweepy import api
-from tweepy.error import TweepError
 
 API_KEY = "cA5q5WNUmtF7S5GTVGRIT9TC0"
 API_SECRET_KEY = "yKVT9sV1pvKe8MgRWiw4L3c1C0ipVp3QVkkZp9CPBhZPcDrVIT"
 ACCESS_TOKEN = "1343829111479828480-xVZF3SwW8cNrbxUPqDvO02y3QPYHKX"
 ACCESS_SECRET_TOKEN = "JqOm8wX4uEPtvQimnouok66xFEte5sjDAqUc5JLA0K5EM"
 
-auth = tweepy.OAuthHandler(API_KEY,API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET_TOKEN)
-api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-art_accounts = ["aic_african","mia_japankorea","cezanneart","SovietArtBot"]
+art_accounts = ["aic_african", "mia_japankorea", "cezanneart", "SovietArtBot"]
+
 
 def retweeter():
-        userId = random.choice(art_accounts)
-        for tweet in tweepy.Cursor(api.user_timeline,id = userId).items():
-            try:
-                if not tweet.retweeted:
-                    tweet.retweet()
-                    print(f"Retweeted {userId}'s tweet")
-                    userId = random.choice(art_accounts)
+    userId = random.choice(art_accounts)
+    for tweet in tweepy.Cursor(api.user_timeline, id=userId).items():
+        try:
+            if not tweet.retweeted:
+                tweet.retweet()
+                print(f"Retweeted {userId}'s tweet")
+                userId = random.choice(art_accounts)
 
-                time.sleep(5)
-                    
-            except Exception as e:
-                print("Error!",e)
-                pass
+            time.sleep(5)
+
+        except Exception as e:
+            print("Error!", e)
+            pass
+
 
 def follower():
     print("retrieving and following follwers...")
@@ -38,12 +39,13 @@ def follower():
                 api.create_friendship(follower.id)
                 print(f"{follower.screen_name} was follwed back!")
         except Exception as e:
-            print("error occured",e)
+            print("error occured", e)
             pass
+
 
 def like_retweeter():
     print("retrieving tweets...")
-    mentions = tweepy.Cursor(api.mentions_timeline,tweet_mode = 'extended').items()
+    mentions = tweepy.Cursor(api.mentions_timeline, tweet_mode="extended").items()
     for mention in mentions:
         if mention.user.id == api.me().id:
             return
@@ -52,32 +54,17 @@ def like_retweeter():
                 mention.favorite()
                 print(f"liked {mention.user.screen_name}'s tweet mentioning you")
             except Exception as e:
-                print("error!",e)
+                print("error!", e)
                 pass
         if not mention.retweeted:
             try:
                 mention.retweet()
                 print(f"retweeted {mention.user.screen_name}'s tweet mentioning you")
             except Exception as e:
-                print("error!",e)
+                print("error!", e)
                 pass
 
-
-
-
-
-            
-
-
-
-    
 
 follower()
 like_retweeter()
 retweeter()
-
-
-    
-
-
-
